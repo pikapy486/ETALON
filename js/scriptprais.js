63453a5f804b4c545b4bbba6777cb4c7fab2fd08
pikapy486 <69073509+pikapy486@users.noreply.github.com>
2022-11-26 14:37:29 +0300

    Add files via upload

--- js/scriptprais.js
+++ js/scriptprais.js
@@ -6,7 +6,7 @@ let navSocial = document.querySelector("#navSocial");
 let hamburger = document.querySelector("#hamburger");
 let nav = document.querySelector("#nav");
 let supportLink = document.querySelector("#support");
-let catalog = document.querySelector("#catalog");
+
 hamburger.onclick = function() {
    hamburger.classList.toggle("hamburger_active");
    navLinks.classList.toggle("nav__links_active");
@@ -22,67 +22,8 @@ supportLink.onclick = function() {
    navSocial.classList.remove("nav__social_active");
    body.classList.remove("scroll_none");
 }
-catalog.onclick = function() {
-   hamburger.classList.remove("hamburger_active");
-   navLinks.classList.remove("nav__links_active");
-   navContacts.classList.remove("nav__contacts_active");
-   navSocial.classList.remove("nav__social_active");
-   body.classList.remove("scroll_none");
-}
 
-
-
-
-
-
-function catalogListAnimation() {
-
-    if (body.clientWidth > 768) {
-       let catalogList = document.querySelector("#catalogList");
-       catalog.addEventListener("mouseover", function() {
-          catalog.classList.add("catalog_open");
-          catalogList.classList.add("catalog__list_open");
-          mainWraper.classList.add("main-wraper_active");
-          body.classList.add("scroll_none");
-       }),
-    
-       catalog.addEventListener("mouseout", function() {
-          catalog.classList.remove("catalog_open");
-          catalogList.classList.remove("catalog__list_open");
-          mainWraper.classList.remove("main-wraper_active");
-          body.classList.remove("scroll_none");
-       });
-       catalogList.addEventListener("mouseover", function() {
-          catalog.classList.add("catalog_open");
-          catalogList.classList.add("catalog__list_open");
-          nav.classList.add("nav_white");
-          mainWraper.classList.add("main-wraper_active");
-          body.classList.add("scroll_none");
-       });
-       catalogList.addEventListener("mouseout", function() {
-          catalog.classList.remove("catalog_open");
-          catalogList.classList.remove("catalog__list_open");
-          mainWraper.classList.remove("main-wraper_active");
-          body.classList.remove("scroll_none");
-          if (window.pageYOffset == 0) {
-             nav.classList.remove("nav_white");
-          }
-             
-       });
-    }
-    
-    
- 
- }
- catalogListAnimation();
-
-
-
-
-
-
-
- function  navShow() {
+function  navShow() {
     let startHeight = window.pageYOffset;
     console.log(startHeight);
     window.onscroll = function() {
